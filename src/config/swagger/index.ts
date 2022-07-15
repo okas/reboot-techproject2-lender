f0720af9d55237c404119d29acf80a901b1115ba
Lauri Saar <lauri.saar@outlook.com>
2022-07-15 17:05:28 +0100

    CHANGE: updated `package.json` info & fixed license
    ADD: Swagger info based on `package.json` file

--- src/config/swagger/index.ts
+++ src/config/swagger/index.ts
@@ -1,10 +1,24 @@
 import { SwaggerOS3Settings } from "@tsed/swagger";
+import {
+  author,
+  description,
+  displayName,
+  license,
+  version
+} from "package.json";
 
 export default [
   {
     path: "/doc",
     specVersion: "3.0.1",
     spec: {
+      info: {
+        title: `REST API documentation of ${displayName}`,
+        version,
+        license: { name: license },
+        contact: author,
+        description
+      },
       components: {
         securitySchemes: {
           jwt: {
