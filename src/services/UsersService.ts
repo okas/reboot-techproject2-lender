054e7c8ae6ee9b38a8778f43d7749b2deb3037b3
Lauri Saar <lauri.saar@outlook.com>
2022-07-16 17:22:23 +0100

    FIX: `lean` option don't return pure class:
    - therefore method: `.toClass()` do not exist.
    
    ADD: authentication purposed query method

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -7,17 +7,24 @@ export class UsersService {
   constructor(@Inject(UserModel) private repo: MongooseModel<UserModel>) {}
 
   async findId(_id: string) {
-    return (await this.repo.findById(_id).lean().exec())?.toClass();
+    return (await this.repo.findById(_id).exec())?.toClass();
   }
 
   async findOne(query: Partial<UserModel>) {
-    return (await this.repo.findOne(query).lean().exec())?.toClass();
+    return (await this.repo.findOne(query).exec())?.toClass();
+  }
+
+  async findForAuth(email: string) {
+    return (
+      await this.repo
+        .findOne({ email })
+        .select({ email: true, password: true })
+        .exec()
+    )?.toClass();
   }
 
   async findSome(query: Partial<UserModel>) {
-    return (await this.repo.find(query).lean().exec())?.map((doc) =>
-      doc.toClass()
-    );
+    return (await this.repo.find(query).exec())?.map((doc) => doc.toClass());
   }
 
   async create(user: UserModel) {
