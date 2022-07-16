ceac0bd38f981d7d2b05d82a1a96eeb529e91dff
Lauri Saar <lauri.saar@outlook.com>
2022-07-17 00:32:14 +0100

    CHANGE: code simplification

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,17 @@
 import { readFileSync } from "fs";
 import { envs } from "./envs/index";
-import loggerConfig from "./logger/index";
-import mongooseConfig from "./mongoose/index";
-import swaggerConfig from "./swagger/index";
-import passportConfig from "./passport/index";
+import logger from "./logger/index";
+import mongoose from "./mongoose/index";
+import swagger from "./swagger/index";
+import passport from "./passport/index";
 
 const pkg = JSON.parse(readFileSync("./package.json", { encoding: "utf8" }));
 
 export const config: Partial<TsED.Configuration> = {
   version: pkg.version,
   envs,
-  logger: loggerConfig,
-  mongoose: mongooseConfig,
-  swagger: swaggerConfig,
-  passport: passportConfig
+  logger,
+  mongoose,
+  swagger,
+  passport,
 };
