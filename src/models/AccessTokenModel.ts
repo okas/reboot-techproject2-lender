75ea710c2f23f014471c18ffef9999b01a60fe3c
Lauri Saar <lauri.saar@outlook.com>
2022-07-18 13:21:15 +0100

    CHANGE: members reorder & options "cleanup" using spread

--- src/models/AccessTokenModel.ts
+++ src/models/AccessTokenModel.ts
@@ -3,19 +3,19 @@ import { Description, Required } from "@tsed/schema";
 import jwt, { Secret, SignOptions } from "jsonwebtoken";
 
 export class AccessTokenModel {
+  @Description("Signed JWT token, for use with `Bearer` schema")
+  @Required()
+  readonly jwt: string;
+
   constructor(user: UserModel, options: SignOptions, secretOrKey: Secret) {
     const signedToken = AccessTokenModel.createJwt(user, options, secretOrKey);
 
     this.jwt = `Bearer ${signedToken}`;
   }
 
-  @Description("Signed JWT token, for use with `Bearer` schema")
-  @Required()
-  readonly jwt: string;
-
   static createJwt(
     { email: subject, roles }: UserModel,
-    options: SignOptions,
+    { ...options }: SignOptions,
     secretOrKey: Secret
   ) {
     return jwt.sign({ roles }, secretOrKey, { subject, ...options });
