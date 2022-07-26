64765d9a2b991766b07091837ba34e902edcf8fa
Lauri Saar <lauri.saar@outlook.com>
2022-07-26 22:46:56 +0100

    ADD: penalty services and controller, by penalty types

---
+++ src/services/FixAmountPenaltyService.ts
@@ -0,0 +1,11 @@
+import { FixAmountPenaltyModel } from "@/models/FixAmountPenaltyModel";
+import { Inject, Service } from "@tsed/di";
+import { MongooseModel } from "@tsed/mongoose";
+import { BaseCRUDService } from "./BaseCRUDService`1";
+
+@Service()
+export class FixAmountPenaltyService extends BaseCRUDService<FixAmountPenaltyModel> {
+  constructor(@Inject(FixAmountPenaltyModel) repository: MongooseModel<FixAmountPenaltyModel>) {
+    super(repository);
+  }
+}
---
+++ src/services/RateOfBaseService.ts
@@ -0,0 +1,11 @@
+import { Inject, Service } from "@tsed/di";
+import { MongooseModel } from "@tsed/mongoose";
+import { RateOfBasePenaltyModel } from "../models/RateOfBasePenaltyModel";
+import { BaseCRUDService } from "./BaseCRUDService`1";
+
+@Service()
+export class RateOfBaseService extends BaseCRUDService<RateOfBasePenaltyModel> {
+  constructor(@Inject(RateOfBasePenaltyModel) repository: MongooseModel<RateOfBasePenaltyModel>) {
+    super(repository);
+  }
+}
