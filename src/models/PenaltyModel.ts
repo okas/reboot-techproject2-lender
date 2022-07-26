b2819b8de5a1448b19fbbe18944fdc74c0a23147
Lauri Saar <lauri.saar@outlook.com>
2022-07-26 17:57:48 +0100

    ADD: "object graph" of Contract Violation, new models: - `ContractViolationsModel`, describes violation, has penalty; - `FixedAmountPenaltyModel`, lookup for specific penalty; - `RateOfBasePenaltyModel`, lookup for specific penalty; - penalties inherit base class that describes discriminator.

---
+++ src/models/PenaltyModel.ts
@@ -0,0 +1,41 @@
+import { BaseModel } from "@/models/BaseModel";
+import { DiscriminatorKey, Model, Trim, Unique } from "@tsed/mongoose";
+import {
+  Default,
+  Description,
+  Example,
+  Ignore,
+  MaxLength,
+  MinLength,
+  Nullable,
+  Required
+} from "@tsed/schema";
+
+@Model({ collection: "penalties" })
+export abstract class BasePenaltyModel extends BaseModel {
+  @Description("Penalty or reason for given violation.")
+  @Example("")
+  @Required()
+  @MinLength(2)
+  @MaxLength(50)
+  @Trim()
+  @Unique()
+  name: string;
+
+  @Description("Non-active penalty cannot be applied to nev Contract Violations.")
+  @Required()
+  @Default(true)
+  active: boolean;
+
+  @Description("Comments")
+  @Example("if there is something to say")
+  @Required(false)
+  @Nullable(String)
+  @MaxLength(100)
+  @Trim()
+  comments?: string;
+
+  @Ignore()
+  @DiscriminatorKey()
+  __t: string;
+}
