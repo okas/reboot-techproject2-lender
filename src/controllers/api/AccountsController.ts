ad4f7e832da87884acbab20ce3df9bd57ef4acf1
Lauri Saar <lauri.saar@outlook.com>
2022-07-27 18:32:19 +0100

    REFACTOR: implemented class `OASDocs` for code cleanup

--- src/controllers/api/AccountsController.ts
+++ src/controllers/api/AccountsController.ts
@@ -2,7 +2,9 @@ import { ShapesEnum } from "@/common/modelShaping";
 import { RolesEnum } from "@/config/authorization";
 import { AuthorizedRoles } from "@/middlewares/AuthorizedRoles";
 import { AccountModel } from "@/models/AccountModel";
+import { BaseModel } from "@/models/BaseModel";
 import { AccountService } from "@/services/AccountService";
+import { OASDocs } from "@/utils/OASDocs";
 import { BodyParams, PathParams } from "@tsed/common";
 import { Controller, Inject } from "@tsed/di";
 import { BadRequest, NotFound } from "@tsed/exceptions";
@@ -20,13 +22,10 @@ import {
   Summary
 } from "@tsed/schema";
 
-const STATUS_404_DESCR = "In case of any incomplete input or input validation failure.";
-const STATUS_400_ID_MISMATCH = "`Id` parameter and `model.id` property mismatch.";
-const STATUS_404 = "Account model not found";
-const get404ForNonExisting = (action: string) => `Not able to ${action} a non-existing account.`;
+const d = new OASDocs("account");
 
 @Controller("/accounts")
-@Description("Accounts management")
+@Description(d.getControllerDecr())
 @Security("jwt")
 @Authenticate("jwt", { session: false })
 @AuthorizedRoles(RolesEnum.LENDER)
@@ -35,7 +34,7 @@ export class AccountsController {
   constructor(@Inject() private service: AccountService) {}
 
   @Get()
-  @Summary("Return all accounts (TO BE PAGINATED!).")
+  @Summary(d.getAllSummary())
   @Status(200, Array).Of(AccountModel)
   async get() {
     // TODO: https://tsed.io/docs/model.html#pagination
@@ -43,25 +42,23 @@ export class AccountsController {
   }
 
   @Get("/:id")
-  @Summary("Return an account by its ID.")
+  @Summary(d.getDocId())
   @Status(200, AccountModel)
-  @Status(404).Description(STATUS_404)
-  async getId(@PathParams() @Required() { id }: never) {
+  @Status(404).Description(d.getNoDoc())
+  async getId(@Description(d.getGetParamId()) @PathParams() @Required() { id }: never) {
     const objModel = await this.service.findById(id);
 
-    if (!objModel) {
-      throw new NotFound("Account not found");
-    }
+    this.assertNotNullish(objModel);
 
     return objModel;
   }
 
   @Post()
-  @Summary("Store a new account.")
-  @Status(201, AccountModel).Description("Stored model instance.")
-  @Status(400).Description(STATUS_404_DESCR)
+  @Summary(d.getPostSummary())
+  @Status(201, AccountModel).Description(d.getPost201StatusDescr())
+  @Status(400).Description(OASDocs.STATUS_404_DESCR)
   async post(
-    @Description("DTO to store new account")
+    @Description(d.getParamPostDtoDescr())
     @BodyParams()
     @Required()
     @Groups(ShapesEnum.CRE)
@@ -73,32 +70,44 @@ export class AccountsController {
   @Put("/:id")
   @Summary("Update an account")
   @Status(204).Description("Updated")
-  @Status(400).Description(STATUS_400_ID_MISMATCH)
-  @Status(404).Description(get404ForNonExisting("update"))
+  @Status(400).Description(OASDocs.STATUS_400_ID_MISMATCH)
+  @Status(404).Description(d.get404ForNonExisting("update"))
   async put(
-    @PathParams() @Required() { id }: never,
-    @BodyParams() @Description("DTO of updated account.") model: AccountModel
+    @Description(d.getParamPutIdDescr()) @PathParams() @Required() { id }: never,
+    @Description(d.getParamPutDtoDescr())
+    @BodyParams()
+    @Description("DTO of updated account.")
+    dto: AccountModel
   ) {
-    if (id !== model._id) {
-      throw new BadRequest(STATUS_400_ID_MISMATCH);
-    }
+    this.assertPutFixIfPossible(id, dto);
 
-    if (!(await this.service.update(model))) {
-      throw new NotFound(STATUS_404);
-    }
+    this.assertNotNullish(await this.service.update(dto));
 
     return;
   }
 
   @Delete("/:id")
-  @Summary("Remove account by ID.")
+  @Summary(d.getDeleteSummary())
   @Status(204).Description("Deleted")
-  @Status(404).Description(get404ForNonExisting("delete"))
+  @Status(404).Description(d.get404ForNonExisting("delete"))
   async delete(@PathParams() @Required() { id }: never) {
-    if (!(await this.service.remove(id))) {
-      throw new NotFound(STATUS_404);
-    }
+    this.assertNotNullish(await this.service.remove(id));
 
     return;
+  }
+
+  //-----------
+  private assertPutFixIfPossible<TModel extends BaseModel>(id: string, dto: TModel) {
+    if (!dto._id) {
+      dto._id = id;
+    } else if (id !== dto._id) {
+      throw new BadRequest(OASDocs.STATUS_400_ID_MISMATCH);
+    }
+  }
+
+  private assertNotNullish<TModel>(doc: TModel) {
+    if (!doc) {
+      throw new NotFound(d.getNoDoc());
+    }
   }
 }
--- src/controllers/api/ContractsController.ts
+++ src/controllers/api/ContractsController.ts
@@ -1,8 +1,10 @@
 import { ShapesEnum } from "@/common/modelShaping";
 import { RolesEnum } from "@/config/authorization";
 import { AuthorizedRoles } from "@/middlewares/AuthorizedRoles";
+import { BaseModel } from "@/models/BaseModel";
 import { ContractModel } from "@/models/ContractModel";
 import { ContractService } from "@/services/ContractService";
+import { OASDocs } from "@/utils/OASDocs";
 import { BodyParams, PathParams } from "@tsed/common";
 import { Controller, Inject } from "@tsed/di";
 import { BadRequest, NotFound } from "@tsed/exceptions";
@@ -20,13 +22,10 @@ import {
   Summary
 } from "@tsed/schema";
 
-const STATUS_404_DESCR = "In case of any incomplete input or input validation failure.";
-const STATUS_400_ID_MISMATCH = "`Id` parameter and `model.id` property mismatch.";
-const STATUS_404 = "Contract model not found";
-const get404ForNonExisting = (action: string) => `Not able to ${action} a non-existing contract.`;
+const d = new OASDocs("contract");
 
 @Controller("/contracts")
-@Description("Contracts management")
+@Description(d.getControllerDecr())
 @Security("jwt")
 @Authenticate("jwt", { session: false })
 @AuthorizedRoles(RolesEnum.LENDER)
@@ -36,32 +35,30 @@ export class ContractsController {
 
   // TODO: https://tsed.io/docs/model.html#pagination
   @Get()
-  @Summary("Return all contracts (TO BE PAGINATED!).")
+  @Summary(d.getAllSummary())
   @Status(200, Array).Of(ContractModel)
   async get() {
     return await this.service.getAll();
   }
 
   @Get("/:id")
-  @Summary("Return a contract by its ID.")
+  @Summary(d.getDocId())
   @Status(200, ContractModel)
-  @Status(404).Description(STATUS_404)
-  async getId(@PathParams() @Required() { id }: never) {
+  @Status(404).Description(d.getNoDoc())
+  async getId(@Description(d.getGetParamId()) @PathParams() @Required() { id }: never) {
     const objModel = await this.service.findById(id);
 
-    if (!objModel) {
-      throw new NotFound("Object model not found");
-    }
+    this.assertNotNullish(objModel);
 
     return objModel;
   }
 
   @Post()
-  @Summary("Store a new contract.")
-  @Status(201, ContractModel).Description("Stored model instance.")
-  @Status(400).Description(STATUS_404_DESCR)
+  @Summary(d.getPostSummary())
+  @Status(201, ContractModel).Description(d.getPost201StatusDescr())
+  @Status(400).Description(OASDocs.STATUS_404_DESCR)
   async post(
-    @Description("DTO to store new contract")
+    @Description(d.getParamPostDtoDescr())
     @BodyParams()
     @Required()
     @Groups(ShapesEnum.CRE)
@@ -71,41 +68,27 @@ export class ContractsController {
   }
 
   @Put("/:id")
-  @Summary("Update a contract")
+  @Summary(d.getPutSummary())
   @Status(204).Description("Updated")
-  @Status(400).Description(STATUS_400_ID_MISMATCH)
-  @Status(404).Description(get404ForNonExisting("update"))
+  @Status(400).Description(OASDocs.STATUS_400_ID_MISMATCH)
+  @Status(404).Description(d.get404ForNonExisting("update"))
   async put(
-    @Description("DTO of updated contract.")
-    @PathParams()
-    @Required()
-    { id }: never,
-    @BodyParams()
-    @Description("Model update DTO")
-    @Groups(ShapesEnum.UPD)
-    model: ContractModel
+    @Description(d.getParamPutIdDescr()) @PathParams() @Required() { id }: never,
+    @Description(d.getParamPutDtoDescr()) @BodyParams() @Groups(ShapesEnum.UPD) dto: ContractModel
   ) {
-    if (!model._id) {
-      model._id = id;
-    } else if (id !== model._id) {
-      throw new BadRequest(STATUS_400_ID_MISMATCH);
-    }
+    this.assertPutFixIfPossible(id, dto);
 
-    if (!(await this.service.update(model))) {
-      throw new NotFound(STATUS_404);
-    }
+    this.assertNotNullish(await this.service.update(dto));
 
     return;
   }
 
   @Delete("/:id")
-  @Summary("Remove contract by ID.")
+  @Summary(d.getDeleteSummary())
   @Status(204).Description("Deleted")
-  @Status(404).Description(get404ForNonExisting("delete"))
+  @Status(404).Description(d.get404ForNonExisting("delete"))
   async delete(@PathParams() @Required() { id }: never) {
-    if (!(await this.service.remove(id))) {
-      throw new NotFound(STATUS_404);
-    }
+    this.assertNotNullish(await this.service.remove(id));
 
     return;
   }
@@ -123,14 +106,26 @@ export class ContractsController {
   @Summary("Return clients contract by its ID.")
   @AuthorizedRoles(RolesEnum.LENDER, RolesEnum.BORROWER)
   @Status(200, ContractModel)
-  @Status(404).Description(STATUS_404)
-  async getClientId(@PathParams() @Required() { id }: never) {
+  @Status(404).Description(d.getNoDoc())
+  async getClientId(@Description(d.getGetParamId()) @PathParams() @Required() { id }: never) {
     const objModel = await this.service.findById(id);
 
-    if (!objModel) {
-      throw new NotFound("Object model not found");
-    }
+    this.assertNotNullish(objModel);
 
     return objModel;
+  }
+  //-----------
+  private assertPutFixIfPossible<TModel extends BaseModel>(id: string, dto: TModel) {
+    if (!dto._id) {
+      dto._id = id;
+    } else if (id !== dto._id) {
+      throw new BadRequest(OASDocs.STATUS_400_ID_MISMATCH);
+    }
+  }
+
+  private assertNotNullish<TModel>(doc: TModel) {
+    if (!doc) {
+      throw new NotFound(d.getNoDoc());
+    }
   }
 }
