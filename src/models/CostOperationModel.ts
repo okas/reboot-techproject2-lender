156ea8de235b98f46a6adb35ea1472e57239ed72
Lauri Saar <lauri.saar@outlook.com>
2022-08-03 11:16:47 +0100

    CHANGE: model validation fixes: - updateOne pre hook receives different params than validate; - therefore added handler mediator handler between core handler and hook;
    CHANGE: improved core validator handler:
    - allow `null|undefined` values;
    - pre-step to ensure that both period props have values if one is present.

--- src/models/CostOperationModel.ts
+++ src/models/CostOperationModel.ts
@@ -1,32 +1,49 @@
 import { ValidationError } from "@tsed/common";
+import { InternalServerError } from "@tsed/exceptions";
 import { Model } from "@tsed/mongoose";
-import { Default, Description, Example, Format, JsonFormatTypes, Required } from "@tsed/schema";
+import { Description, Example, Format, JsonFormatTypes, Nullable } from "@tsed/schema";
 import { BaseCostModel } from "./BaseCostModel";
 
 @Model({
   discriminatorValue: "op-cost",
   pre: [
+    // { method: "findOneAndUpdate", fn: CostOperationModel.validateHook },
     { method: "validate", fn: CostOperationModel.validateHook },
-    { method: "findOneAndUpdate", fn: CostOperationModel.validateHook },
-    { method: "updateOne", fn: CostOperationModel.validateHook }
+    { method: "updateOne", fn: CostOperationModel.updateOneHook }
   ]
 })
 export class CostOperationModel extends BaseCostModel {
-  @Description("Period start.")
+  @Description("Period start, if set then `periodEnd` must be set as well.")
   @Example(Date.now)
-  @Required()
-  @Default(Date.now)
+  @Nullable(Date)
   @Format(JsonFormatTypes.DATE)
-  periodStart: Date;
+  periodStart?: Date;
 
-  @Description("Period end.")
+  @Description("Period end, if set then `periodStart` must be set as well.")
   @Example(Date.now)
-  @Required()
-  @Default(Date.now)
+  @Nullable(Date)
   @Format(JsonFormatTypes.DATE)
-  periodEnd: Date;
+  periodEnd?: Date;
+
+  static async updateOneHook({ _update }: { _update: CostOperationModel }) {
+    if (!_update) {
+      throw new InternalServerError(
+        "Validation failure: updateOne hook didn't receive expected object to validate."
+      );
+    }
+
+    return CostOperationModel.validateHook(_update);
+  }
 
   static async validateHook({ periodStart, periodEnd }: CostOperationModel) {
+    if (!periodStart && !periodEnd) {
+      return;
+    }
+
+    if (!periodStart || !periodEnd) {
+      throw new ValidationError("If period is need, both start and end must be specified.");
+    }
+
     if (periodStart > periodEnd) {
       throw new ValidationError("Start period cannot be later than end period.");
     }
