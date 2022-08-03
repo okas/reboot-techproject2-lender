11cd207bdf9a789f4eee05249003b73e1b3eec90
Lauri Saar <lauri.saar@outlook.com>
2022-08-03 17:05:38 +0100

    ADD: `UserModel` ref pre-check for existence

--- src/services/CostBorrowerService.ts
+++ src/services/CostBorrowerService.ts
@@ -1,6 +1,9 @@
+import { ShapesEnum } from "@/common/ShapesEnum";
 import { AccountModel } from "@/models/AccountModel";
 import { CostBorrowerModel } from "@/models/CostBorrowerModel";
+import { UserModel } from "@/models/UserModel";
 import { Inject, Service } from "@tsed/di";
+import { NotFound } from "@tsed/exceptions";
 import { MongooseModel } from "@tsed/mongoose";
 import { BaseCostService } from "./common/BaseCostService";
 
@@ -8,8 +11,29 @@ import { BaseCostService } from "./common/BaseCostService";
 export class CostBorrowerService extends BaseCostService<CostBorrowerModel> {
   constructor(
     @Inject(CostBorrowerModel) repository: MongooseModel<CostBorrowerModel>,
-    @Inject(AccountModel) repoAccount: MongooseModel<AccountModel>
+    @Inject(AccountModel) repoAccount: MongooseModel<AccountModel>,
+    @Inject(UserModel) private repoUser: MongooseModel<UserModel>
   ) {
     super(repository, repoAccount);
+  }
+
+  async create(dto: CostBorrowerModel): Promise<CostBorrowerModel> {
+    await this.tryVerifyBorrowerOrThrow(dto.borrower.toString(), ShapesEnum.CRE);
+
+    return super.create(dto);
+  }
+
+  async update(dto: CostBorrowerModel): Promise<number> {
+    await this.tryVerifyBorrowerOrThrow(dto.borrower.toString(), ShapesEnum.UPD);
+
+    return super.update(dto);
+  }
+
+  private async tryVerifyBorrowerOrThrow(_id: string, action: string) {
+    const borrowerExist = await this.repoUser.countDocuments({ _id }).exec();
+
+    if (!borrowerExist) {
+      throw new NotFound(`Cannot ${action} cost: unknown borrower.`);
+    }
   }
 }
