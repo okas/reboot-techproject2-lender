3341c47bc578175a3c25d41f0b3f667634e15ba7
Lauri Saar <lauri.saar@outlook.com>
2022-08-04 13:27:20 +0100

    FIX: added missing doc & validation info

--- src/models/BasePortfolioTransactionModel.ts
+++ src/models/BasePortfolioTransactionModel.ts
@@ -1,10 +1,16 @@
 import { AccountModel } from "@/models/AccountModel";
-import { DiscriminatorKey, Model, Ref } from "@tsed/mongoose";
-import { Description, Ignore, Required } from "@tsed/schema";
+import { DiscriminatorKey, Model, Ref, Trim } from "@tsed/mongoose";
+import { Description, Example, Ignore, MaxLength, MinLength, Required } from "@tsed/schema";
 import { BaseTransaction } from "./common/BaseTransaction";
 
 @Model({ collection: "portfolio-transactions" })
 export abstract class BasePortfolioTransactionModel extends BaseTransaction {
+  @Description("Other party of transaction.")
+  @Example("Arbitrary descriptor")
+  @Required()
+  @MinLength(2)
+  @MaxLength(50)
+  @Trim()
   channel: string;
 
   @Description("Account reference")
