15f0e64a4bd3c60b93f512eb327f5d229cd62f9c
Lauri Saar <lauri.saar@outlook.com>
2022-08-04 19:36:32 +0100

    ADD: `ContractExistsInterceptor`

---
+++ src/interceptors/model-validation/ContractExistsInterceptor.ts
@@ -0,0 +1,15 @@
+import { ContractModel } from "@/models/ContractModel";
+import { Inject, Interceptor } from "@tsed/di";
+import { MongooseModel } from "@tsed/mongoose";
+import { BaseRefExistsInterceptor } from "./BaseRefExistsInterceptor";
+import { ExistenceInterceptorOpts } from "./ExistenceInterceptorOpts";
+
+@Interceptor()
+export class ContractExistsInterceptor extends BaseRefExistsInterceptor<
+  ContractModel,
+  ExistenceInterceptorOpts
+> {
+  constructor(@Inject(ContractModel) repo: MongooseModel<ContractModel>) {
+    super(repo);
+  }
+}
