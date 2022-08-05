86b67a14f25dc8bf621bdce24d57a200370436ea
Lauri Saar <lauri.saar@outlook.com>
2022-08-05 20:33:01 +0100

    CHANGE: implemented `PenaltyExistsInterceptor`: - also refactored for code reuse with nameofFactory;

--- src/services/ContractViolationService.ts
+++ src/services/ContractViolationService.ts
@@ -1,12 +1,16 @@
 import { ContractExistsInterceptor } from "@/interceptors/model-validation/ContractExistsInterceptor";
 import { ExistenceInterceptorOpts } from "@/interceptors/model-validation/ExistenceInterceptorOpts";
+import { PenaltyExistsInterceptor } from "@/interceptors/model-validation/PenaltyExistsInterceptor";
 import { ContractViolationModel } from "@/models/ContractViolationModel";
-import { nameof } from "@/utils/nameof-helpers";
+import { nameofFactory } from "@/utils/nameof-helpers";
 import { Inject, Intercept, Service } from "@tsed/di";
 import { MongooseModel } from "@tsed/mongoose";
 import { BaseCRUDService } from "./common/BaseCRUDService`1";
 
-const options: ExistenceInterceptorOpts = { key: nameof<ContractViolationModel>("contract") };
+const nameof = nameofFactory<ContractViolationModel>();
+
+const contractOptions: ExistenceInterceptorOpts = { key: nameof("contract") };
+const penaltyOptions: ExistenceInterceptorOpts = { key: nameof("penalty") };
 
 @Service()
 export class ContractViolationService extends BaseCRUDService<ContractViolationModel> {
@@ -14,12 +18,14 @@ export class ContractViolationService extends BaseCRUDService<ContractViolationM
     super(repo);
   }
 
-  @Intercept(ContractExistsInterceptor, options)
+  @Intercept(PenaltyExistsInterceptor, penaltyOptions)
+  @Intercept(ContractExistsInterceptor, contractOptions)
   async create(dto: Partial<ContractViolationModel>): Promise<ContractViolationModel> {
     return (await this.repo.create(dto)).toClass();
   }
 
-  @Intercept(ContractExistsInterceptor, options)
+  @Intercept(PenaltyExistsInterceptor, penaltyOptions)
+  @Intercept(ContractExistsInterceptor, contractOptions)
   async update(dto: Partial<ContractViolationModel>): Promise<number> {
     return (await this.repo.updateOne({ _id: dto._id }, dto)).matchedCount;
   }
