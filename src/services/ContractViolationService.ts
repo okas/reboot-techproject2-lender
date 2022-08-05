76b013a1f13b11ef37e83f323e588e725c3bbfc3
Lauri Saar <lauri.saar@outlook.com>
2022-08-05 19:57:44 +0100

    CHANGE: implemented `ContractExistsInterceptor`

--- src/services/ContractViolationService.ts
+++ src/services/ContractViolationService.ts
@@ -1,11 +1,26 @@
+import { ContractExistsInterceptor } from "@/interceptors/model-validation/ContractExistsInterceptor";
+import { ExistenceInterceptorOpts } from "@/interceptors/model-validation/ExistenceInterceptorOpts";
 import { ContractViolationModel } from "@/models/ContractViolationModel";
-import { Inject, Service } from "@tsed/di";
+import { nameof } from "@/utils/nameof-helpers";
+import { Inject, Intercept, Service } from "@tsed/di";
 import { MongooseModel } from "@tsed/mongoose";
 import { BaseCRUDService } from "./common/BaseCRUDService`1";
 
+const options: ExistenceInterceptorOpts = { key: nameof<ContractViolationModel>("contract") };
+
 @Service()
 export class ContractViolationService extends BaseCRUDService<ContractViolationModel> {
-  constructor(@Inject(ContractViolationModel) repository: MongooseModel<ContractViolationModel>) {
-    super(repository);
+  constructor(@Inject(ContractViolationModel) repo: MongooseModel<ContractViolationModel>) {
+    super(repo);
+  }
+
+  @Intercept(ContractExistsInterceptor, options)
+  async create(dto: Partial<ContractViolationModel>): Promise<ContractViolationModel> {
+    return (await this.repo.create(dto)).toClass();
+  }
+
+  @Intercept(ContractExistsInterceptor, options)
+  async update(dto: Partial<ContractViolationModel>): Promise<number> {
+    return (await this.repo.updateOne({ _id: dto._id }, dto)).matchedCount;
   }
 }
