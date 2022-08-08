17b6f6777ba8e294e09d97fb763e03a3d7bfafef
Lauri Saar <lauri.saar@outlook.com>
2022-08-08 13:55:53 +0100

    CHANGE: `contractId` pathparam validation: - added param to POST and PUT actions to validate against DTO; - moved `assertContractIdEquals` from `BaseContractTransactionController` to composable function.

--- src/controllers/api/common/BaseContractTransactionController.ts
+++ src/controllers/api/common/BaseContractTransactionController.ts
@@ -1,23 +1,14 @@
 import { BaseContractTransactionModel } from "@/models/BaseContractTransactionModel";
-import { BadRequest } from "@tsed/exceptions";
 import { BaseController } from "./BaseController";
+import { assertContractIdEquals } from "./contract-path-helpers";
 
 export abstract class BaseContractTransactionController extends BaseController {
-  protected assertContractIdEquals<TModel extends BaseContractTransactionModel>(
-    contractId: string,
-    { contract }: TModel
-  ) {
-    if (contractId !== contract) {
-      throw new BadRequest("Contract id mismatch.");
-    }
-  }
-
   protected assertPutFixIfPossible<TModel extends BaseContractTransactionModel>(
     contractId: string,
     id: string,
     dto: TModel
   ) {
-    this.assertContractIdEquals(contractId, dto);
+    assertContractIdEquals(contractId, dto);
 
     BaseController.assertPutFixIfPossible(id, dto);
   }
